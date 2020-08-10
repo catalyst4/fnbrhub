d26735a2606e0f913016c5cd2e5dd674af295817
jakestanex <jaykstanex@gmail.com>
2020-08-10 16:06:29 +0100

    fixes

--- routes/admin/editMap.js
+++ routes/admin/editMap.js
@@ -13,22 +13,22 @@ router.post('/admin/edit', ensureAuth, async (req,res) => {
 
     }
 
-    let id;
     let linkId;
+    let link;
 
     if(youtubeLink.includes('https://www.youtube.com/watch?v=')) {
-        id = youtubeLink.split('https://www.youtube.com/watch?v=');
-        linkId = id[1];
+        link = youtubeLink.split('https://www.youtube.com/watch?v=');
+        linkId = link[1];
     }
 
     if(youtubeLink.includes('https://youtube.com/watch?v=')) {
-        id = youtubeLink.split('https://youtube.com/watch?v=');
-        linkId = id[1];
+        link = youtubeLink.split('https://youtube.com/watch?v=');
+        linkId = link[1];
     }
 
     if(youtubeLink.includes('https://youtu.be/')) {
-        id = youtubeLink.split('https://youtu.be/');
-        linkId = id[1];
+        link = youtubeLink.split('https://youtu.be/');
+        linkId = link[1];
     }
 
     try {
